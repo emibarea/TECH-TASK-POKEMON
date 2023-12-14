6fb6784b106f47f278028124d6afe14c238dfcc8
Emiliano Barea <emilianobarea8@gmail.com>
2023-12-14 15:22:32 -0300

    posible fix to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+import type { InlineConfig } from "vitest";
+import type { UserConfig } from "vite";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+} as VitestConfigExport);
